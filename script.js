f377b535a2f175aedaeca84253d2ef5a0a49c2b0
Kumari G <151625325+kumari01@users.noreply.github.com>
2025-03-17 18:27:45 +0530

    script.js

--- script.js
+++ script.js
@@ -7,6 +7,30 @@ var typed = new Typed(('#skill'),{
 document.querySelector("form").addEventListener("submit", function(event) {
     setTimeout(() => {
         this.reset();
-    }, 1000); // Reset form after 1 second to ensure submission
+    }, 1000);
 });
 
+function toggleMenu() {
+    const navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+}
+window.addEventListener('scroll', function () {
+    const sections = document.querySelectorAll('section'); 
+    const navLinks = document.querySelectorAll('.navbar a'); 
+
+    let currentSection = ''; 
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${currentSection}`) {
+            link.classList.add('active');
+        }
+    });
+});
